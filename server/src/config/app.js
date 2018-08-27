c87b28324622c09c5e82fb548c11348e23db3c3d
Laszlo Szekely-Toth <notisracer@gmail.com>
2018-08-27 11:37:31 +0200

    client-hot-reload renamed to client-hmr and calling config now located in app.js

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
+import configClientHmr from './client-hmr.js';
+if (process.env.NODE_ENV === 'development') {
+    configClientHmr(app);
+}
+
 app.get('/', function (req, res) {
     console.log(path.resolve(__dirname, 'index.html'));
     res.sendFile(path.resolve(__dirname, 'index.html'));
